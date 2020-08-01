961e280a4957d1ad3f5f40dc2adf6cccadb4b301
Git Test <ashwinkolkar22@gmail.com>
2020-08-02 00:40:48 +0530

    first commit

---
+++ src/com/TestGit/TheTesting.java
@@ -0,0 +1,11 @@
+package com.TestGit;
+
+public class TheTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello This is the Ashwin");
+	}
+
+}
